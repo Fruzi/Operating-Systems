aef751b76fec24f695676fb52209c94c4038d9be
Ron M <ronmad@post.bgu.ac.il>
2018-06-01 18:04:21 +0300

    Fix byte counts and prints for sanitytest

--- sanitytest.c
+++ sanitytest.c
@@ -4,29 +4,30 @@
 #include "fcntl.h"
 #include "fs.h"
 
-int main(int argc, char **argv) {
+int main() {
   int fd;
-  int i, n, total;
-  char buf[BSIZE] = {0};
+  int i, n;
+  int total = 0;
+  char buf[BSIZE];
+  memset(buf, 'a', BSIZE);
 
   if ((fd = open("textfile", O_CREATE|O_RDWR)) < 0) {
     printf(1, "error: open\n");
     exit();
   }
-  total = 0;
-  for(i = 0; i < 1954; i++){
-    if((n = write(fd, buf, BSIZE)) != BSIZE){
+  for (i = 0; i < (1 << 20) / BSIZE; i++) {
+    if ((n = write(fd, buf, BSIZE)) != BSIZE) {
       printf(1, "error: write\n");
       exit();
     }
     total += n;
     if (total == BSIZE * NDIRECT) {
-      printf(1, "Finished writing %dKB (direct)\n", total / 1000);
+      printf(1, "Finished writing %dKB (direct)\n", total >> 10);
     } else if (total == BSIZE * (NDIRECT + NINDIRECT)) {
-      printf(1, "Finished writing %dKB (indirect)\n", total / 1000);
+      printf(1, "Finished writing %dKB (single indirect)\n", total >> 10);
     }
   }
-  printf(1, "Finished writing %dMB\n", total / 1000000);  
+  printf(1, "Finished writing %dMB\n", total >> 20);
   close(fd);
   exit();
 }
